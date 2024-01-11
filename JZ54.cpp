4c44baa9ee14f04b2e9b6138a4c294ebbdefafcb
GitGitHubGiteeGitLab <130532493+GitGitHubGiteeGitLab@users.noreply.github.com>
2024-01-11 13:05:50 +0800

    二叉搜索树的第k个节点

--- JZ54.cpp
+++ JZ54.cpp
@@ -18,19 +18,19 @@ public:
      */
     /**
         BST: 左小右边大， 压到一条直线上就是一个升序序列
-        Preorder: 能把BST压平到一条直线上
+        inorder: 能把BST压平到一条直线上
     */
-    void preorder(TreeNode *root, vector<int>& ans){
+    void inorder(TreeNode *root, vector<int>& ans){
         if(root){
-            preorder(root->left, ans);
+            inorder(root->left, ans);
             ans.push_back(root->val);
-            preorder(root->right, ans);
+            inorder(root->right, ans);
         }
     }
     int KthNode(TreeNode* proot, int k) {
         // write code here
         vector<int> ans;
-        preorder(proot, ans);
+        inorder(proot, ans);
         if(!proot || k > ans.size() || k <= 0){
             return -1;
         }
