ebcb6b5d67236ced64e1b6931a2df9f77b1053a0
GitGitHubGiteeGitLab <130532493+GitGitHubGiteeGitLab@users.noreply.github.com>
2024-01-11 13:38:09 +0800

    二叉树的镜像

---
+++ JZ27.cpp
@@ -0,0 +1,31 @@
+/**
+ * struct TreeNode {
+ *	int val;
+ *	struct TreeNode *left;
+ *	struct TreeNode *right;
+ *	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * };
+ */
+class Solution {
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param pRoot TreeNode类 
+     * @return TreeNode类
+     */
+    TreeNode* Mirror(TreeNode* pRoot) {
+        // write code here
+        /**
+            swap(left, right);
+            分别处理左右子树
+        */
+        if(pRoot){
+            swap(pRoot->left, pRoot->right);
+            Mirror(pRoot->left);
+            Mirror(pRoot->right);
+        } 
+        return pRoot;     
+    }
+};
